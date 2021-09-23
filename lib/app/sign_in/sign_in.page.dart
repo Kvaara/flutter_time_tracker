68a721c77e7ccf3093f2ec0552ce70c35dd5272e
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-09-23 04:47:09 +0300

    Created a new directory called 'widgets' where I will put every custom widget that I create. For example, I created a CustomElevatedButton Widget that's very flexible, reusable and in addition is more readable and saves some lines of code.

--- lib/app/sign_in/sign_in.page.dart
+++ lib/app/sign_in/sign_in.page.dart
@@ -1,22 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_time_tracker/widgets/elevated_button.custom.dart';
 
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("lol"),
-          ),
-          elevation: 3.0,
+      appBar: AppBar(
+        title: const Center(
+          child: Text("lol"),
         ),
-        body: _buildContent());
+        elevation: 3.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey.shade200,
+    );
   }
 
   Widget _buildContent() {
     return Container(
-      margin: EdgeInsets.all(12.0),
+      margin: const EdgeInsets.all(12.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -32,12 +35,21 @@ class SignInPage extends StatelessWidget {
           const SizedBox(
             height: 8,
           ),
-          const ElevatedButton(
-            onPressed: null,
-            child: Text(
-              "Sign in through Google",
+          CustomElevatedButton(
+            child: const Text(
+              "Sign in with Google",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 1,
+              ),
             ),
-          )
+            buttonBackgroundColor: Colors.white,
+            onPressed: () {
+              print("lol");
+            },
+          ),
         ],
       ),
     );
