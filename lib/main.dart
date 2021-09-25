794eb741b4a33c67b7c78f8f0e8fb670845be361
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-09-25 21:00:52 +0300

    Configured both the android level and app level build.gradle files to support Firebase (added the required packages). Added the Firebase Core and Firebase Auth packages as libraries from pub.dev (We can now use Firebase with Dart). Lastly, I ran the initializeApp method before the runApp method because firebase needs to be initialized before the widget tree is built.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import "package:flutter/material.dart";
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app/sign_in/sign_in.page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Time Tracker",
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: SignInPage());
+      title: "Time Tracker",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const SignInPage(),
+    );
   }
 }
