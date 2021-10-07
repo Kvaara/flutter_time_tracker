16323c7d0ee62c9f425648b72c538661fc0d60ad
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-10-07 07:51:36 +0300

    Added the google sign in library form pub.dev to the project. Now we can easily start an interactive Google sign-in process. I created added a new behavior into the Auth class called signInWithGoogle that handles and validates the user if they choose to sign in with Google.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: file_names
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthInterface {
   User? get currentUser;
   Stream<User?> authStateChanges();
   Future<User?> signInAnonymously();
+  Future<User> signInWithGoogle();
   Future<void> signOut();
 }
 
@@ -21,6 +23,33 @@ class Auth implements AuthInterface {
   Future<User?> signInAnonymously() async {
     final userCredential = await _firebaseAuthInstance.signInAnonymously();
     return userCredential.user;
+  }
+
+  @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuthInstance
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        ));
+        return userCredential.user!;
+      } else {
+        throw FirebaseAuthException(
+          code: "ERROR_MISSING_GOOGLE_ID_TOKEN",
+          message: "Missing the Google ID token.",
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: "ERROR_ABORTED_BY_USER",
+        message: "Sign in aborted by the user.",
+      );
+    }
   }
 
   @override
